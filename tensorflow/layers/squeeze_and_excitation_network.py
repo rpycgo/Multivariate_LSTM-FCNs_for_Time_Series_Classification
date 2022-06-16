fix: Debug global average pooling layer

tensorflow/layers/squeeze_and_excitation_network.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-from tensorflow.keras.layers import Layer, GlobalAveragePooling1D, Dense, Conv1D, RepeatVector
+from tensorflow.keras.layers import Layer, Dense, Conv1D
 
 
 class SqueezeAndExcitationNetworks(Layer):
@@ -11,10 +11,9 @@ class SqueezeAndExcitationNetworks(Layer):
         filters=x.shape[-1]
 
         u = Conv1D(filters=filters, kernel_size=1)(x)   # batch_size, time_seq, filters
-        average_pooling = GlobalAveragePooling1D()(u)   # batch_size, filters
-        fc1 = Dense(units=(filters // self.reduction_ratio), activation='relu')(average_pooling)    # batch_size, filters
-        fc2 = Dense(units=filters, activation='sigmoid')(fc1)   # batch_size, filters
+        _global_average_pooling = tf.reduce_mean(u, axis=(0, 1))   # filters
+        global_average_pooling = _global_average_pooling[tf.newaxis, tf.newaxis, :]   # 1, 1, filters
+        fc1 = Dense(units=(filters // self.reduction_ratio), activation='relu')(global_average_pooling)    # 1, 1, filters/reduction_ratio
+        fc2 = Dense(units=filters, activation='sigmoid')(fc1)   # 1, 1, filters
 
-        fc2_repeated = RepeatVector(x.shape[1])(fc2)    # batch_size, time_seq, filters
-
-        return u * fc2_repeated
+        return u * fc2